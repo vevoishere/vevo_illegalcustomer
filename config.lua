b4b9a61df84e195b11fe569b74c35d01652405fc
vevoishere <68850718+vevoishere@users.noreply.github.com>
2020-07-27 13:43:36 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,67 @@
+-- Coded by Vevo#3838
+
+Config = {
+	
+	MPEDS = {
+		-1382092357,
+		-9308122,
+		-1463670378,
+		766375082,
+		-106498753,
+		-795819184,
+		1704428387,
+		2064532783,
+		-264140789,
+		706935758,
+		349505262,
+		1459905209,
+		1346941736,
+		-162605104,
+	},
+	
+	CARS = {
+		330661258,
+		-2095439403,
+		349315417,
+		523724515,
+		1645267888,
+		1373123368,
+		349605904,
+		1507916787,
+	},
+	
+	Black = true,
+	
+	MinClockHour = 23,
+    MaxClockHour = 05,
+	
+	Product = {
+	
+		weed_pooch = {
+			label = 'Paketlenmiş Esrar',
+			sellprice = math.random(4000, 4800),
+			minumum = 5,
+		},
+		
+		WEAPON_NIGHTSTICK = {
+			label = 'Jop',
+			sellprice = math.random(100, 500),
+			minumum = 5,
+		},
+		
+		WEAPON_STUNGUN = {
+			label = 'Stun Gun',
+			sellprice = math.random(2300, 3000),
+			minumum = 1,
+		},
+		
+		drone_flyer_7 = {
+			label = 'Polis Dronu',
+			sellprice = math.random(200, 1000),
+			minumum = 5,
+		},
+		
+	},
+	
+	
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+server_scripts {
+	'server.lua',
+	'config.lua',
+}
+
+client_scripts {
+	'client.lua',
+	'config.lua',
+}
